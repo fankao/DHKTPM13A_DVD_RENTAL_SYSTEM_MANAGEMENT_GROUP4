558d7dee8ebaa97d5a996fde6119714ee1edeaae
chien <minhchiendc@gmail.com>
2020-12-08 10:29:46 +0700

    Cap nhat tim kiem tua de

--- src/main/java/com/group4/business/TuaDeBUS.java
+++ src/main/java/com/group4/business/TuaDeBUS.java
@@ -1,37 +1,42 @@
 package com.group4.business;
-import static com.group4.Injection.*;
-import com.group4.dao.IChiTietDatGiuDAO;
-import com.group4.dao.IDiaDAO;
-import com.group4.dao.ITuaDeDAO;
-import com.group4.dao.impl.ChiTietDatGiuDAO;
-import com.group4.dao.impl.DiaDAO;
-import com.group4.dao.impl.TuaDeDAO;
+
+import static com.group4.Injection.chiTietDatGiuDAO;
+import static com.group4.Injection.diaDAO;
+import static com.group4.Injection.tuaDeDAO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.group4.entities.TuaDe;
 
 public class TuaDeBUS {
 	/**
 	 * Xóa tựa đề
+	 * 
 	 * @param tuaDe: Tựa đề cần xóa
-	 * @return true: nếu xóa tựa đề thành công/ false: nếu tựa đề có trong đĩa, đang được đặt giữ 
+	 * @return true: nếu xóa tựa đề thành công/ false: nếu tựa đề có trong đĩa, đang
+	 *         được đặt giữ
 	 */
-	
+
 	public boolean xoaTuaDe(TuaDe tuaDe) {
-		if(tuaDeCoDia(tuaDe.getId())==true || 
-				tuaDeDaDatGiu(tuaDe.getId())==true) {
+		if (tuaDeCoDia(tuaDe.getId()) == true || tuaDeDaDatGiu(tuaDe.getId()) == true) {
 			return false;
 		}
 		return tuaDeDAO.delete(tuaDe);
 	}
-	
+
 	public boolean tuaDeCoDia(Long tuaDeId) {
-		return diaDAO.findAll().stream().
-				filter(x -> x.getTuaDe().getId() == tuaDeId).count() > 0;
+		return diaDAO.findAll().stream().filter(x -> x.getTuaDe().getId() == tuaDeId).count() > 0;
 	}
-	
+
 	public boolean tuaDeDaDatGiu(Long tuaDeId) {
-		return chiTietDatGiuDAO.findAll().stream()
-				.filter(x -> x.getTuaDe().getId() == tuaDeId).count() > 0;
+		return chiTietDatGiuDAO.findAll().stream().filter(x -> x.getTuaDe().getId() == tuaDeId).count() > 0;
 	}
-	
-	
+
+	public List<TuaDe> timKiemTuaDe(String tenTuaDe) {
+		return tuaDeDAO.findAll().stream().filter(x -> x.getTenTuaDe().trim().toLowerCase().contains(tenTuaDe))
+				.collect(Collectors.toList());
+
+	}
+
 }
